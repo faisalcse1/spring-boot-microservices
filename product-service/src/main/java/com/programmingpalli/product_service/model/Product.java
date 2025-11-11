502368ff658db8afc7896ed62f1c64a81511f864
Md Faisal <faisalcse1@gmail.com>
2025-11-11 20:54:35 +0600

    product model has been added

---
+++ product-service/src/main/java/com/programmingpalli/product_service/model/Product.java
@@ -0,0 +1,21 @@
+package com.programmingpalli.product_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Document(value = "product")
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private double price;
+}
